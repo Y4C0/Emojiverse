109835112f96770baa4b6d2abbb6a9a0ef8e5819
Yarden <jc0h3n@gmail.com>
2022-08-01 21:47:58 +0300

    added profile

--- app.py
+++ app.py
@@ -25,11 +25,13 @@ def create_app():
     app.secret_key = 'cairocoders-ednalan'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-    APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-    DB_HOST = "localhost"
-    DB_NAME = "postgres"
-    DB_USER = "postgres"
-    DB_PASS = "123456"
+    return app
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+DB_HOST = "localhost"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASS = "123456"
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'mp4'])
